a89b5e8c716d6bebf4aa21f8f188eb45301b6a1c
Dual <93845583+Dual-28@users.noreply.github.com>
2025-11-18 22:20:41 +0000

    Delete lua directory

--- lua/solitudegang/test/frame.lua
+++
@@ -1,9 +0,0 @@
-SolitudeGang.Tests.Frame = function()
-    local frame = vgui.Create("SolitudeGang.Frame")
-    frame:SetSize(800, 600)
-    frame:Center()
-    frame:MakePopup()
-    frame:SetTitle("SolitudeGang Frame Test")
-end
-
-concommand.Add("solitudegang_test_frame", SolitudeGang.Tests.Frame)
